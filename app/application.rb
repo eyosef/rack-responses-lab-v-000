44f9eae5c4fa79aed409fef93588fb23bae903fd
Eden Mesfun <emesfun@gmail.com>
2018-03-20 14:07:25 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     resp = Rack::Response.new
     time = Time.now
 
-    if time.hour < 12
+    if Time.now.hour < 12
       resp.write "Morning"
     else
       resp.write "Afternoon"
